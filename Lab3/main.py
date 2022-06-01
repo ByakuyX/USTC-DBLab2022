f26c6de047218b445a92b3817748dacc0e51d07f
白夜 <106675046+ByakuyX@users.noreply.github.com>
2022-06-02 00:06:06 +0800

    Delete Lab3 directory

--- Lab3/main.py
+++
@@ -1,9 +0,0 @@
-from src.Menu import Menu
-from PyQt5.QtWidgets import QApplication
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    a = Menu()
-    a.show()
-    sys.exit(app.exec_())
